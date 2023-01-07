33f3cea5eac9acb72175745c830baed1d61d30a4
Sp3ctroR3tro <76521319+Sp3ctroR3tro@users.noreply.github.com>
2023-01-07 16:51:43 -0500

    Connect_4
    Added additional game logic and board functionality

--- Connect_4.py
+++ Connect_4.py
@@ -1,5 +1,4 @@
 # Installing needed imports
-
 import numpy as np
 import pygame
 import sys
@@ -25,14 +24,21 @@ def create_board():
     board = np.zeros((6,7))
     return board
 
-def drop_piece():
-    None
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
 
-def is_valid_location(col):
-    None
+def is_valid_location(board, col):
+    board[BOARD_ROWS - 1][col] == 0
 
-def get_next_open_row(col):
-    None
+    
+
+def get_next_open_row(board, col):
+    for r in range(BOARD_ROWS):
+        if board[r][col] == 0:
+            return r 
+
+def print_board(board):
+    print(np.flip(board, 0))
 
 # Initializing the game board
 board = create_board()
@@ -47,6 +53,10 @@ while not game_over:
         col = int(input("Player One's Turn"))
     else:
         col = int(input("Player Two's Turn"))
+    
+    print_board(board)
+
+
     turn += 1 
     turn % 2
 
